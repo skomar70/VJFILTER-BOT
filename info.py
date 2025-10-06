33322e3363caa80c5706444a85ba43d693ef4eea
skomar70 <mosharofroydev@gmail.com>
2025-10-06 11:08:53 +0000

    Update info.py

--- info.py
+++ info.py
@@ -90,7 +90,7 @@ PUBLIC_FILE_CHANNEL = environ.get('PUBLIC_FILE_CHANNEL', '') # Public Channel Us
 # Links
 GRP_LNK = environ.get('GRP_LNK', 'https://t.me/BotWinnerGroup')
 CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/vjotz')
-SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'vj_botdisscussion') # Support Chat Link Without https:// or @
+SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'https://t.me/request_gruop') # Support Chat Link Without https:// or @
 OWNER_LNK = environ.get('OWNER_LNK', 'https://t.me/BotWinnerGroup')
 
 # True Or False
@@ -196,6 +196,7 @@ else:
 # Don't Remove Credit @VJ_Botz
 # Subscribe YouTube Channel For Amazing Bot @Tech_VJ
 # Ask Doubt on telegram @KingVJ01
+
 
 
 
